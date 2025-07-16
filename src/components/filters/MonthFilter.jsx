ac7c7ff2009f1f31327fa445899d4086a917f40b
unknown <danrlei_vieira@hotmail.com>
2025-07-16 11:29:58 -0300

    refactor: fix MonthFilter to pass month index as value for correct filtering

--- src/components/filters/MonthFilter.jsx
+++ src/components/filters/MonthFilter.jsx
@@ -2,6 +2,21 @@ import { useState } from "react";
 
 function MonthFilter({ onMonthChange }) {
   const [selectMonth, setSelectMonth] = useState("");
+  const monthName = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const currentMonth = new Date().getMonth();
 
   const handleChange = (e) => {
     const newMonth = e.target.value;
@@ -19,18 +34,12 @@ function MonthFilter({ onMonthChange }) {
         onChange={handleChange}
         required
       >
-        <option value="0">Jan</option>
-        <option value="1">Feb</option>
-        <option value="2">Mar</option>
-        <option value="3">Apr</option>
-        <option value="4">May</option>
-        <option value="5">Jun</option>
-        <option value="6">Jul</option>
-        <option value="7">Aug</option>
-        <option value="8">Sep</option>
-        <option value="9">Oct</option>
-        <option value="10">Nov</option>
-        <option value="11">Dec</option>
+        <option value="">Select month</option>
+        {monthName.slice(0, currentMonth + 1).map((month, index) => (
+          <option key={index} value={index}>
+            {month}
+          </option>
+        ))}
       </select>
     </>
   );

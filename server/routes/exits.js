02dc3d469a07c8e8595d343dedee2b31d67e1f0b
unknown <danrlei_vieira@hotmail.com>
2025-08-18 20:45:37 -0300

    feat: add exits routes to fetch data from the database

---
+++ server/routes/exits.js
@@ -0,0 +1,23 @@
+import express from "express";
+import pool from "../db.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT *  FROM extis ORDER BY date DESC");
+
+    const formattedExits = result.rows.map((exit) => ({
+      ...exit,
+      date: exit.date.toISOString().split("T")[0],
+      amount: Number(exit.amount),
+    }));
+
+    res.json(formattedExits);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Erro ao buscar dados" });
+  }
+});
+
+export default router;

84e90bac89cd25a6a2d70acd41db3af8c11bf4dd
unknown <danrlei_vieira@hotmail.com>
2025-09-09 20:23:47 -0300

    style: adjust frame order and improve information styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,22 +13,22 @@ function Home() {
 
   return (
     <div className="flex flex-row flex-wrap justify-center items-center w-full min-w-[340px] text-xs sm:text-base h-auto mb-8 gap-2">
-      <div className="flex flex-col justify-between items-center mb-4 border-box w-[300px] h-auto shadow-2xl shadow-tertiary">
+      <div className="bg-primary text-black flex flex-col justify-between items-center mb-4 border-box w-[300px] h-auto shadow-2xl shadow-tertiary">
         <h2 className="h2-bold mb-6">Month current</h2>
 
-        <div className="box-info mb-4">
-          <p className="font-bold text-xl">Current balance:</p>
-          <p className="text-blue-500 font-bold text-xl">R$ +5.000,00</p>
+        <div className="box-info mb-4 border-2 border-tertiary p-2 bg-slate-300">
+          <p className="font-bold text-xl">Entry</p>
+          <p className="text-green-600 font-bold text-xl">R$ 4.000,00</p>
         </div>
 
-        <div className="box-info mb-4">
-          <p className="font-bold text-xl">Entry:</p>
-          <p className="text-green-600 font-bold text-xl">R$ + 4.000,00</p>
+        <div className="box-info mb-4 border-2 border-tertiary p-2 bg-slate-300">
+          <p className="font-bold text-xl">Exit</p>
+          <p className="text-red-600 font-bold text-xl">R$ -1.000,00</p>
         </div>
 
-        <div className="box-info mb-4">
-          <p className="font-bold text-xl">Exit:</p>
-          <p className="text-red-600 font-bold text-xl">R$ - 1.000,00</p>
+        <div className="box-info mb-4 border-2 border-tertiary p-2 bg-slate-300">
+          <p className="font-bold text-xl">Current balance</p>
+          <p className="text-blue-500 font-bold text-xl">R$ 5.000,00</p>
         </div>
       </div>
 
@@ -38,19 +38,6 @@ function Home() {
             Phrase of the day
           </h2>
           <p className="p-4 text-center italic text-xl font-bold">{phrase}</p>
-        </div>
-      </div>
-
-      <div className="border-box shadow-2xl shadow-tertiary">
-        <div>
-          <h2 className="h2-bold box-info mb-6">Chart of the month</h2>
-          <div className="border-solid border-2 border-tertiary ">
-            <img
-              src={graficoDinheiro}
-              className="grafic"
-              alt="Gráfico Mensal"
-            />
-          </div>
         </div>
       </div>
 
@@ -98,6 +85,19 @@ function Home() {
         <a href="/summary" className="text-blue-500 font-bold underline">
           See all
         </a>
+      </div>
+
+      <div className="border-box shadow-2xl shadow-tertiary">
+        <div>
+          <h2 className="h2-bold box-info mb-6">Chart of the month</h2>
+          <div className="border-solid border-2 border-tertiary ">
+            <img
+              src={graficoDinheiro}
+              className="grafic"
+              alt="Gráfico Mensal"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );

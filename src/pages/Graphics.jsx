c215bcb7c775df6996e652b64495b653997b1a51
unknown <danrlei_vieira@hotmail.com>
2025-09-23 14:37:38 -0300

    style: enhance layout responsiveness across breakpoints

--- src/pages/Graphics.jsx
+++ src/pages/Graphics.jsx
@@ -44,64 +44,89 @@ function Graphics() {
   const chartData = getDateByMonth(entries, exits, year);
 
   return (
-    <main className="flex flex-col items-center w-full min-w-[340px] text-xs sm:text-base mb-20">
-      <h1 className="text-4xl font-bold mb-4 text-center my-4">Graphics</h1>
+    <main className="flex flex-col items-center w-full min-w-[340px] text-xs sm:text-base mb-20 lg:ml-[280px]">
+      <h1 className="text-4xl font-bold mb-4 text-center my-4 w-full lg:mr-[280px]">Graphics</h1>
+
+      {/*Filtros */}
+      <div className="lg:fixed lg:top-40 lg:right-2 z-40">
+        <div className="border-box sm:p-1 lg:p-4 md:m-0 m-2 w-auto h-auto flex flex-col items-center justify-center">
+          <h2 className="font-bold text-xl">Period filter</h2>
+
+          <div className="flex flex-col w-auto h-auto border-2 border-tertiary gap-2 p-2 m-1">
+            <button className="bg-green-600 text-white p-2 rounded w-auto h-[30px] flex items-center justify-center active:bg-green-800">
+              Quarter
+            </button>
+            <button className="bg-green-600 text-white p-2 rounded w-auto h-[30px] flex items-center justify-center  active:bg-green-800">
+              Semester
+            </button>
+            <button className="bg-green-600 text-white p-2 rounded w-auto h-[30px] flex items-center justify-center active:bg-green-800">
+              Year
+            </button>
+          </div>
+        </div>
+      </div>
 
       {/* div container graphics*/}
-      <div className="relative flex sm:flex-row flex-col flex-wrap items-center justify-center w-full sm:p-2 p-0 gap-4 sm:pr-[320px] mb-8">
-        <div className="border-box w-auto h-auto flex flex-col items-center justify-center shadow-2xl shadow-tertiary">
-          <h2 className="font-bold text-2xl box-info">
-            Inflows and Outflows per month
-          </h2>
+      <div className="relative flex md:flex-row flex-col flex-wrap items-center justify-center w-full sm:p-2 p-0 gap-4 lg:pr-[320px] mb-8">
+        <div className="border-4 border-tertiary md:p-2 rounded w-auto h-auto flex flex-col items-center justify-center shadow-2xl shadow-tertiary">
+          <div className="w-[80%]">
+            <h2 className="font-bold sm:text-2xl text-xl box-info text-center">
+              Inflows and Outflows per month
+            </h2>
+          </div>
 
-          <div className="flex w-[600px] h-auto m-4 border-2 border-black">
-            <ResponsiveContainer width="100%" height={300}>
-              <LineChart
-                data={chartData}
-                margin={{ top: 10, right: 40, left: 10, bottom: 40 }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="month">
-                  <Label value="Month" offset={-40} position="insideBottom" />
-                </XAxis>
-                <YAxis
-                  domain={[0, "dataMax"]}
-                  tickCount={9}
-                  tickFormatter={(value) => `${value.toLocaleString("pt-BR")}`}
-                />
-                <Tooltip
-                  formatter={(value, name) => [
-                    `R$ ${value.toLocaleString("pt-BR", {
-                      minimumFractionDigits: 2,
-                    })}`,
-                    name,
-                  ]}
-                />
-                <Legend
-                  verticalAlign="top"
-                  align="center"
-                  wrapperStyle={{ padding: 0 }}
-                />
-                <Line
-                  type="linear"
-                  dataKey="entries"
-                  stroke="green"
-                  activeDot={{ r: 8 }}
-                />
-                <Line type="monotone" dataKey="exits" stroke="red" />
-                <ReferenceLine
-                  y={5000}
-                  label="Meta"
-                  stroke="blue"
-                  strokeDasharray="3 3"
-                />
-                <Brush dataKey="month" height={20} stroke="blue"/>
-              </LineChart>
-            </ResponsiveContainer>
+          <div className="flex max-w-[260px] md:max-w-[450px] h-auto sm:m-4 border-2 border-black overflow-x-auto">
+            <div className="min-w-[500px]">
+              <ResponsiveContainer width="100%" height={300}>
+                <LineChart
+                  data={chartData}
+                  margin={{ top: 10, right: 40, left: 10, bottom: 40 }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="month">
+                    <Label value="Month" offset={-40} position="insideBottom" />
+                  </XAxis>
+                  <YAxis
+                    domain={[0, "dataMax"]}
+                    tickCount={9}
+                    tickFormatter={(value) =>
+                      `${value.toLocaleString("pt-BR")}`
+                    }
+                  />
+                  <Tooltip
+                    formatter={(value, name) => [
+                      `R$ ${value.toLocaleString("pt-BR", {
+                        minimumFractionDigits: 2,
+                      })}`,
+                      name,
+                    ]}
+                  />
+                  <Legend
+                    verticalAlign="top"
+                    align="center"
+                    wrapperStyle={{ padding: 0 }}
+                  />
+                  <Line
+                    type="linear"
+                    dataKey="entries"
+                    stroke="green"
+                    activeDot={{ r: 8 }}
+                  />
+                  <Line type="monotone" dataKey="exits" stroke="red" />
+                  <ReferenceLine
+                    y={5000}
+                    label={{ value: "Meta", position: "top", fill: "blue" }}
+                    stroke="blue"
+                    strokeDasharray="3 3"
+                  />
+                  <Brush dataKey="month" height={20} stroke="blue" />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
           </div>
 
           <div>
-            <button className="bg-blue-500 p-0.5 rounded w-auto flex items-center active:bg-blue-800 border-collapse border-2 border-tertiary gap-1">
+            <button className="bg-blue-500 p-0.5 my-4 rounded w-auto flex items-center active:bg-blue-800 border-collapse border-2 border-tertiary gap-1">
               <FaFilePdf /> Export PDF
             </button>
           </div>
@@ -133,24 +158,6 @@ function Graphics() {
           <div>
             <button className="bg-blue-500 p-0.5 rounded w-auto flex items-center active:bg-blue-800 border-collapse border-2 border-tertiary gap-1">
               <FaFilePdf /> Export PDF
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div className="sm:fixed sm:top-40 sm:right-4 z-40">
-        <div className="border-box p-4 w-auto h-auto flex flex-col items-center justify-center">
-          <h2 className="font-bold text-xl">Filtro de periodo</h2>
-
-          <div className="flex flex-col w-auto h-auto border-2 border-tertiary gap-2 p-2 m-1">
-            <button className="bg-green-600 text-white p-2 rounded w-auto h-[30px] flex items-center justify-center active:bg-green-800">
-              Quarter
-            </button>
-            <button className="bg-green-600 text-white p-2 rounded w-auto h-[30px] flex items-center justify-center  active:bg-green-800">
-              Semester
-            </button>
-            <button className="bg-green-600 text-white p-2 rounded w-auto h-[30px] flex items-center justify-center active:bg-green-800">
-              Year
             </button>
           </div>
         </div>
